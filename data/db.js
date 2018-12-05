0bfbcf17ee2a9f2b2d3114f9d1c552b5703978ec
prerna-p <prernasocialid@gmail.com>
2018-12-05 15:45:09 -0500

    question schema updated

--- data/db.js
+++ data/db.js
@@ -1,8 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'whiteboard-cs5610-fall-2018';
-    var   connectionString =
-        'mongodb://localhost/';
+    var connectionString = 'mongodb://localhost/';
     connectionString += databaseName;
     mongoose.connect(connectionString);
 };
