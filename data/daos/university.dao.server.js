5c725ec1ce3d88173b9045aca0e877b770765505
prerna-p <prernasocialid@gmail.com>
2018-12-06 10:09:31 -0500

    working on populating db

--- data/daos/university.dao.server.js
+++ data/daos/university.dao.server.js
@@ -3,30 +3,42 @@ const questionModel = require('../models/question.model.server');
 const answerModel = require('../models/answer.model.server');
 
 // removes all the data from the database. Note that you might need to remove documents in a particular order
-/*
-truncateDatabase = () = {
 
+truncateDatabase = () => {
+    answerModel.remove({});
+    questionModel.remove({});
+    studentModel.remove({});
 }
-*/
+
+populateStudent = (id,uname,pass,fn,ln,gy,sc) => {
+    let student = {
+        _id:id,
+        username:uname,
+        password:pass,
+        firstName:fn,
+        lastName:ln,
+        gradYear:gy,
+        scholarship:sc
+    }
+    return student;
+}
 
 // populates the database with test data as described in a later section
 populateDatabase = () => {
+    createStudent(populateStudent(123,'alice','alice','Alice','Wonderland',2020,15000));
+    createStudent(populateStudent(123,'alice','alice','Alice','Wonderland',2020,15000))
 
 };
 
+
+
+
+
 // inserts a student document
 createStudent = student =>
-    studentModel.save(student);
-    /*studentModel.create({
-        _id: student.id,
-        username: student.username,
-        password: student.password,
-        firstName: student.firstName,
-        lastName: student.lastName,
-        gradYear: student.gradYear,
-        scholarship: student.scholarship
+    studentModel.create(student).then(newUser =>
+        console.log(newUser));
 
-    })*/
 
 //inserts a question document
 createQuestion = (question) => {
@@ -106,3 +118,16 @@ module.exports = {
     findAnswersByStudent,
     findAnswersByQuestion
 };
+
+
+
+/*studentModel.create({
+    _id: student.id,
+    username: student.username,
+    password: student.password,
+    firstName: student.firstName,
+    lastName: student.lastName,
+    gradYear: student.gradYear,
+    scholarship: student.scholarship
+
+})*/
